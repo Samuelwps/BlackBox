b655637e970fa3b8672d4392df407407f81d0f5a
Samuel Ribeiro <samuelrip230@gmail.com>
2025-04-19 16:07:23 -0300

    #

--- src/Components/4-Reviews/index.jsx
+++ src/Components/4-Reviews/index.jsx
@@ -18,21 +18,21 @@ function Reviews() {
       text: "Eu li um dos eBooks em uma tarde e já mudei minha forma de pensar no trabalho. Resultado? Fui promovido 2 meses depois. Vale cada centavo!",
       name: "Bruno L., 32 anos, Gerente Comercial",
       date: "cliente desde 2025",
-      image: "/images/avatar/avatar1.jpg", // Caminho direto para a pasta public
+      image: "/images/avatar/avatar1.webp", // Caminho direto para a pasta public
     },
     {
       id: 2,
       text: "Sempre achei que desenvolvimento pessoal era enrolação… até começar a aplicar as técnicas simples que aprendi aqui. Em 3 semanas, meus resultados dobraram. Incrível!",
       name: "Tatiane R., 28 anos, Empreendedora",
       date: "cliente desde 2022",
-      image: "/images/avatar/avatar2.jpg", // Caminho direto para a pasta public
+      image: "/images/avatar/avatar2.webp", // Caminho direto para a pasta public
     },
     {
       id: 3,
       text: "Eu nunca tive o hábito de ler, mas esses eBooks são diferentes. Linguagem direta, conteúdo prático e transformação real. Hoje tenho mais foco, mais disciplina e muito mais autoconfiança.",
       name: "Carlos M., 24 anos, Estudante de Engenharia",
       date: "cliente desde 2023",
-      image: "/images/avatar/avatar3.jpg", // Caminho direto para a pasta public
+      image: "/images/avatar/avatar3.webp", // Caminho direto para a pasta public
     },
   ];
 
